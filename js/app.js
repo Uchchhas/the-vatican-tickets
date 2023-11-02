c8579b0c1baeb72225c7457e2213972622bad432
Uchchhas <msruchchhas.dev@gmail.com>
2023-11-02 18:18:31 +0600

    Home page running

--- js/app.js
+++ js/app.js
@@ -64,6 +64,29 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+/* -- Swiper Initialization -- */
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    height: 344,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
 // JQuery Code Starts
 $(document).ready(function () {
     $('select').niceSelect();
