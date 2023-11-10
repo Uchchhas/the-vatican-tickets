ef22dc820f0d6b0541eea138715d2c98d4402d48
Uchchhas <msruchchhas.dev@gmail.com>
2023-11-10 19:24:36 +0600

    Booking page running

--- js/booking.js
+++ js/booking.js
@@ -1,4 +1,70 @@
 document.addEventListener('DOMContentLoaded', function () {
+    /* -- Booking Wizard -- */
+    const wizardStepButtons = document.querySelectorAll('.booking-wizard .wizard-menu li'),
+        wizardSteps = document.querySelectorAll('.wizard-step'),
+        wizardStepNextButtons = document.querySelectorAll('.wizard-step .btn-next'),
+        wizardStepLoader = document.querySelector('.wizard-step-loader');
+
+    let currentStep = 3;
+    let activeStep = null;
+
+    wizardStepButtons.forEach(item => {
+        const wizardNumber = item?.getAttribute('data-wizard-number');
+
+        if (Number(wizardNumber) < currentStep) {
+            item?.classList.add('completed');
+        }
+
+        if (Number(wizardNumber) === currentStep) {
+            item?.classList.add('current');
+        }
+
+        if (item?.classList.contains('completed')) {
+            const wizardNumber = item?.querySelector('.wizard-number');
+            wizardNumber.innerHTML = `<i class="fas fa-check"></i>`;
+        }
+    });
+
+    wizardSteps.forEach(item => {
+        const stepNumber = item?.getAttribute('data-step-number');
+
+        if (Number(stepNumber) === currentStep) {
+            activeStep = item;
+        }
+    });
+
+    wizardStepNextButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const nextStep = btn.getAttribute('data-next-step');
+            showStepLoader();
+            wizardSteps.forEach(item => {
+                item.style.display = 'none';
+                if (Number(item.getAttribute('data-step-number')) === Number(nextStep)) {
+                    activeStep = item;
+                }
+            });
+            setTimeout(() => {
+                hideStepLoader();
+                activeStep.style.display = 'block';
+                document.documentElement.scrollTop = 0;
+            }, 1000);
+        });
+    });
+
+    const showStepLoader = () => {
+        wizardStepLoader.style.display = 'flex';
+    }
+
+    const hideStepLoader = () => {
+        wizardStepLoader.style.display = 'none';
+    }
+
+    setTimeout(() => {
+        hideStepLoader();
+        activeStep.style.display = 'block';
+    }, 500)
+
+
     /* -- Booking Calendar Initialization -- */
     const options = {
         type: 'multiple',
@@ -36,6 +102,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const calendar = new VanillaCalendar('#calendar', options);
     calendar.init();
+
 
     /* -- Initialize Preference Slider -- */
     const swiperPreference = new Swiper('.swiper-preference', {
